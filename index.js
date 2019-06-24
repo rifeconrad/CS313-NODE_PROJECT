488f632f76b66fd98c7aa8f3956c5af4504c47d8
rifeconrad <rif16001@byui.edu>
2019-06-24 13:43:24 -0400

    commit

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ express()
 	    }
 
 	    if (result.rows.length == 1) {
-	    	console.log(result);
-	    	res.write(result.rows[0].first_name);
+	    	console.log(result.rows[0].first_name);
 	    } 
 	}); 
   })
