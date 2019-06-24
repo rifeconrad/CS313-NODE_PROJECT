2f2ac8765c13514ba7ac11cd1eacbe9de0b14a32
rifeconrad <rif16001@byui.edu>
2019-06-24 06:33:58 -0400

    commit

--- index.js
+++ index.js
@@ -2,26 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var Pool = require('pg-pool')
-const connectionString = process.env.DATABASE_URL || 'postgres://yironekfbpmahc:ae09277254f5e43817b7b8672dd253d2221aad218a26e7191c741da11d15de70@ec2-174-129-242-183.compute-1.amazonaws.com:5432/deilk6ur1bg4m5?ssl=true'
-
-const pool = new Pool({connectionString: connectionString});
-
-var sql = "SELECT * FROM USERS";
-
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
-
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result.rows);
-
-
-});     
+ 
 
 
 express()
@@ -29,4 +10,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/list', (req.res) => res.render('pages/list'))
+  .get('/create'. (req, res) => res.render('pages/create'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
