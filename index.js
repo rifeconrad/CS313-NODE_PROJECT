4fcffa99e9d30cc1bec058970924960eb4e2e690
rifeconrad <rif16001@byui.edu>
2019-06-24 06:49:43 -0400

    commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const bodyParser = require('body-parser');
  
 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({ extended: false }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/list', function(req, res) {
-
-
+   console.log(req.body);
 
    res.render('pages/list')
   })
