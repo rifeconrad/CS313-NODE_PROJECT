1dafa73def643bc7041d1b2c5f615f0ff9892e6e
rifeconrad <rif16001@byui.edu>
2019-06-24 08:21:23 -0400

    commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var Pool = require('pg-pool')
 const connectionString = process.env.DATABASE_URL || 'postgres://yironekfbpmahc:ae09277254f5e43817b7b8672dd253d2221aad218a26e7191c741da11d15de70@ec2-174-129-242-183.compute-1.amazonaws.com:5432/deilk6ur1bg4m5?ssl=true'
 const pool = new Pool({connectionString: connectionString});
 
+var user_verified = true;
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({ extended: false }))
@@ -14,25 +16,39 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/list', function(req, res) {
-   const uname = req.body.uname;
-   const pwrd = req.body.pwrd;
+    const uname = req.body.uname;
+    const pwrd = req.body.pwrd;
 
-	var sql = "SELECT * FROM USERS";
+	verifyUser(uname, pwrd);
+    if (user_verified) {
+    	console.log("rendering pages list");
+    	res.render('pages/list');
+    } else {
+    	console.log("rendering pages login");
+    	res.render('pages/index');
+    }
+  })
+  .get('/create', (req, res) => res.render('pages/create'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-	pool.query(sql, function(err, result) {
+function setUserVerification(verified) {
+	user_verified = verified;
+}
+
+function verifyUser(uname, pswrd, callback) {
+	var sql = "SELECT * FROM USERS WHERE username=$1::string AND password=$2::string";
+	var params = [uname, pswrd];
+	pool.query(sql, params, function(err, result) {
 	    // If an error occurred...
 	    if (err) {
 	        console.log("Error in query: ")
 	        console.log(err);
 	    }
 
-	    // Log this to the console for debugging purposes.
-	    console.log("Back from DB with result:");
-	    console.log(result.rows);
-	});  
-
-
-   res.render('pages/list')
-  })
-  .get('/create', (req, res) => res.render('pages/create'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	    if (result.rows.length == 1) {
+	    	console.log("FOUND ROW");
+	    	callback(true);
+	    }
+	    callback(false);
+	}); 
+}
